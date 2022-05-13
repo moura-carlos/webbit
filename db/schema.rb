d5c9c30bfcccfb6dda711bf339bf3df41e9d9b99
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-05-12 21:03:54 -0300

    Add video attachment support and user assotiations to submissions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_220119) do
+ActiveRecord::Schema.define(version: 2022_05_12_225702) do
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2022_05_12_220119) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "submission_image"
+    t.string "submission_video"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_05_12_220119) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submissions", "users"
 end
