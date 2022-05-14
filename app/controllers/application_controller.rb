7a8c915f11166e509dc0a97b85b792cb623b5e7e
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-05-14 19:34:59 -0300

    Extend devise to account for a username field, create migration to add new username column to the users database, add User model validations to ensure a user cannot be created without a username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # added attributes
+    # this code essencially allows the user to signup with the user name and edit their profile
+    # with their user name
+    added_attrs = [:username]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
