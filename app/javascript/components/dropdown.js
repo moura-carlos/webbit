a305ae6de08a0b09af1a51df11f279fce11d7b76
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-05-15 20:47:40 -0300

    UI enhancements and JavaScript dropdowns ajustments, change in the dropdown component CSS

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,5 +1,45 @@
 document.addEventListener("turbolinks:load", () => {
-  const dropdown = document.querySelector(".js-dropdown-user-target");
+  const dropDownLinks = document.querySelectorAll(".js-nav .dropdown a");
+  const dropDownMenu = document.querySelector(".dropdown-menu");
+
+
+  function dropdown(){
+    dropDownLinks.forEach(function(dropdown) {
+      dropdown.addEventListener("click", (e) => {
+        const dropdownTarget = dropdown.nextElementSibling;
+        if (e.target.nextElementSibling != null){
+          // Hide the dropdown
+          dropdownTarget.classList.toggle("hidden");
+          // Click outside dropdown to also close
+          hideOnClickOutside(dropdown);
+          e.preventDefault();
+        }
+      })
+    });
+  }
+
+  function hideOnClickOutside(element) {
+    const outsideClickListener = function(event) {
+      if(!element.contains(event.target)) {
+        if (!!element && !!(element.offsetWidth || element.offsetHeight || element.getClientRects().length)) {
+          element.nextElementSibling.classList.toggle('hidden');
+          removeClickListener();
+        }
+      }
+    }
+    const removeClickListener = function() {
+      document.removeEventListener("click", outsideClickListener);
+    }
+
+    document.addEventListener("click", outsideClickListener);
+  }
+
+  function init() {
+    dropdown();
+  }
+
+  init();
+  /*const dropdown = document.querySelector(".js-dropdown-user-target");
   const dropdownList = document.querySelector(".dropdown-list");
 
   dropdown.addEventListener("click", (e) => e.preventDefault());
@@ -12,5 +52,5 @@ document.addEventListener("turbolinks:load", () => {
       // remove dropdown if no target is the dropdown element
       dropdownList.classList.add("hidden");
     }
-  });
+  });*/
 });
