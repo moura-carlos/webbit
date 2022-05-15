b3e7d13126c18873c7ca26f94993754e4851ee25
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-05-15 18:11:27 -0300

    Refactor global community dropdown layout to show actual community names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # basically means: before this controller loads,
+  # go ahead and find and execute the find_communites method.
+  # thus it'll give us access to all the communities in our app.
+  before_action :find_communities
+
 
   protected
+
+  def find_communities
+    @communities = Community.all.order(:title)
+  end
+
   def configure_permitted_parameters
     # added attributes
     # this code essencially allows the user to signup with the user name and edit their profile
