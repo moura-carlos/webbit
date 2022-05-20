e315a921dbde0e4589a50c1515647570c08fa406
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-05-19 22:07:18 -0300

    Account for video, images, and texts on submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,7 @@
 class Submission < ApplicationRecord
   include VotesCountable
   mount_uploader :submission_image, SubmissionImageUploader
-  mount_uploader :submission_video, SubmissionImageUploader
+  mount_uploader :submission_video, SubmissionVideoUploader
 
   belongs_to :user
   belongs_to :community
@@ -11,6 +11,26 @@ class Submission < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { maximum: 8000 }
   validates :url, url: true, allow_blank: true
+  validate :image_or_video
+  validate :url_or_content
 
   acts_as_votable
+
+  private
+
+  def image_or_video
+    unless submission_image.blank? || submission_video.blank?
+      unless submission_image.blank? ^ submission_video.blank?
+        errors.add(:base, 'Add an image or video, not both')
+      end
+    end
+  end
+
+  def url_or_content
+    unless url.blank? || body.blank?
+      unless url.blank? ^ body.blank?
+        errors.add(:base, 'Add a valid URL or text content')
+      end
+    end
+  end
 end
